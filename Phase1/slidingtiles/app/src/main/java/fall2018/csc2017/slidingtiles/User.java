0c02db0dad823d9589d9687a77e0898001a6f680
perrinni <nick.perrin@mail.utoronto.ca>
2018-11-06 00:43:48 -0500

    Removed obsolete pieces of code again from a rogue commit

--- Phase1/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/User.java
+++ Phase1/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/User.java
@@ -10,7 +10,6 @@ public class User implements Serializable {
     private String account;
     private String password;
     private String gameFile;
-    private static int heighest_score = 9999999;
 
 
     public User(String account, String password) {
@@ -28,10 +27,4 @@ public class User implements Serializable {
     }
 
     public String getPassword() { return password; }
-
-    public int getHeighest_score() { return heighest_score; }
-
-    public void setHeighest_score(int s) {
-        this.heighest_score = s;
-    }
 }
