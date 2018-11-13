4b81788bd769d1cc715125c4555f27670d2d8e83
perrinni <nick.perrin@mail.utoronto.ca>
2018-11-13 17:00:59 -0500

    Trying to fix again

---
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/LaunchCenterActivity.java
@@ -0,0 +1,76 @@
+package fall2018.csc2017.slidingtiles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+
+/**
+ * The launch center activity.
+ */
+public class LaunchCenterActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_launchcenter);
+        addChooseButtonListener();
+    }
+
+    /**
+     * Activate the choose button.
+     */
+    private void addChooseButtonListener() {
+        Button chooseButton = findViewById(R.id.choosebutton);
+        chooseButton.setOnClickListener(this::popup);
+        setupLogOutListener();
+    }
+
+    /**
+     * Pop up a menu of games.
+     */
+    public void popup(View v) {
+        PopupMenu pop = new PopupMenu(this, v);
+        pop.setOnMenuItemClickListener(this);
+        pop.inflate(R.menu.game_menu);
+        pop.show();
+    }
+
+    /**
+     * Activate the items in the menu.
+     */
+    public boolean onMenuItemClick(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.game1:
+                switchToSlidingTile();
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Switch to the sliding tile starting acticity.
+     */
+    private void switchToSlidingTile() {
+        Intent tmp = new Intent(this, StartingActivity.class);
+        startActivity(tmp);
+    }
+
+    /**
+     * Activate the logout button.
+     */
+    public void setupLogOutListener(){
+        Button signIn = findViewById(R.id.logout);
+        signIn.setOnClickListener((v) -> {
+            Intent tmp = new Intent(this, RegisterActivity.class);
+            startActivity(tmp);
+        });
+    }
+}
+
---
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/NoPassWordException.java
@@ -0,0 +1,11 @@
+package fall2018.csc2017.slidingtiles;
+
+/**
+ *  A NoPasswordException Class.
+ */
+
+class NoPassWordException extends Throwable {
+    public NoPassWordException(){
+        super();
+    }
+}
