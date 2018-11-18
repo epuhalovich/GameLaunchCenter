99cd0d32bdae2406fb5ab788800d9857dc4d59a4
perrinni <nick.perrin@mail.utoronto.ca>
2018-11-17 21:14:13 -0500

    First JUnit test file added

---
+++ Phase2/slidingtiles/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingTilesScoreboard_Test.java
@@ -0,0 +1,19 @@
+package fall2018.csc2017.slidingtiles;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit tests for the sliding tiles scoreboard.
+ *
+ * Following methods at:
+ * - http://d.android.com/tools/testing
+ * - https://developer.android.com/training/testing/unit-testing/local-unit-tests
+ */
+public class SlidingTilesScoreboard_Test {
+    // TODO: Write tests
+}
+
