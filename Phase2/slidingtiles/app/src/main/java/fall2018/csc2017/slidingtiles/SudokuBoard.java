7c70fd33146232763d0a15ca9e0ff48444fb3fbd
chenkey8 <kkeying.chen@mail.utoronto.ca>
2018-11-21 20:41:03 -0500

    Sudoku start code

--- Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/SudokuBoard.java
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/SudokuBoard.java
@@ -1,84 +1,134 @@
 package fall2018.csc2017.slidingtiles;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class SudokuBoard {
-    private ArrayList<ArrayList<String>> newSudoku;
-    private final String[][] seedSudoku = {
+    private Random rand = new Random();
+    public  List<List<String>> listSudoku;
+    private final String[][][] seedSudokus = {{
             {"1", "2","3","4","5","6","7","8","9"},
-    {"4","5","6","7","8","9","1","2","3"},
-        {"7","8","9","1","2","3","4","5","6"},
+            {"4","5","6","7","8","9","1","2","3"},
+            {"7","8","9","1","2","3","4","5","6"},
             {"2","1","4","3","6","5","8","9","7"},
-                {"3","6","5","8","9","7","2","1","4"},
-                    {"8","9","7","2","1","4","3","6","5"},
-                        {"5","3","1","6","4","2","9","7","8"},
-                            {"6","4","2","9","7","8","5","3","1"},
-                                {"9","7","8","5","3","1","6","4","2"},
-};
-    private String[][] sample = seedSudoku;
+            {"3","6","5","8","9","7","2","1","4"},
+            {"8","9","7","2","1","4","3","6","5"},
+            {"5","3","1","6","4","2","9","7","8"},
+            {"6","4","2","9","7","8","5","3","1"},
+            {"9","7","8","5","3","1","6","4","2"}},
+            {{"3","9","4","5","1","7","6","2","8"},
+                    { "5", "1", "7", "6", "2", "8", "3", "9", "4" },
+                    { "6", "2", "8", "3", "9", "4", "5", "1", "7" },
+                    { "9", "3", "5", "4", "7", "1", "2", "8", "6" },
+                    { "4", "7", "1", "2", "8", "6", "9", "3", "5" },
+                    { "2", "8", "6", "9", "3", "5", "4", "7", "1" },
+                    { "1", "4", "3", "7", "5", "9", "8", "6", "2" },
+                    { "7", "5", "9", "8", "6", "2", "1", "4", "3" },
+                    { "8", "6", "2", "1", "4", "3", "7", "5", "9" }}};
 
-    //create a new sudoku based on the sample by swapping two numbers.
 
-
-
-
+    public SudokuBoard(){
+        this.listSudoku = getListSampleSudoku(getNewSudoku());
+    }
 
     // create a new sudoku based on the sample by swapping rows and cols by 20 times.
-    private String[][] getNewSudokuA(String[][] Sudoku){
-        swap(Sudoku, "r");
-        swap(Sudoku, "c");
-       return Sudoku;
+    private String[][] getNewSudoku(){
+        ArrayList<String[][]> matrixs = new ArrayList<>();
+        String[][] sample = getSampleSudoku();
+        for(int times = 0; times != 10; times++){
+            matrixs.add(swapColumn(sample));
+            matrixs.add(swapDia(sample));
+            matrixs.add(swapLine(sample));
+            matrixs.add(swapNumber(sample));
+        }
+        return matrixs.get(rand.nextInt(matrixs.size()));
     }
 
-    //swap two number of the seedSudoku.
-    private void swapNumber(String[][] seed, String n, String m){
 
-    }
-
-    private void swap(String[][] seed, String ways){
-        for (int i = 0; i < 21; i++){
-            Random rand = new Random();
-            int row = rand.nextInt(8);
-            int col = rand.nextInt(8);
-            if(ways.equals("r")){
-                int changeRow = validChange(row);
-                String a = seed[row][col];
-                seed[row][col] = seed[changeRow][col];
-                seed[changeRow][col] = a;}
-            else{
-                int changeCol = validChange(col);
-                String a = seed[row][col];
-                seed[row][col] = seed[row][changeCol];
-                seed[row][changeCol] = a;
-
+    //swap two number in the diagnal
+    private String[][] swapDia(String[][] sample) {
+        String[][] dia = new String[9][9];
+        for (int i = 0; i != 9; i++) {
+            for (int j = 0; j != 9; j++) {
+                dia[i][j] = sample[j][i];
             }
         }
+        return dia;
+    }
+    //swap two number of the seedSudoku.
+    private String[][] swapNumber(String[][] sample){
+        for(int times = 0; times != 8; times ++){
+            String changeNum = Integer.toString(rand.nextInt(9) + 1);
+            String Num = Integer.toString(rand.nextInt(9) + 1);
+            for(int i = 0; i != 9; i++){
+                for(int j = 0; j!=9; j++){
+                    if(sample[i][j].equals(Num)){
+                        sample[i][j] = changeNum;
+                    }
+                    else if(sample[i][j].equals(changeNum)){
+                        sample[i][j] = Num;
+                    }
+                }
+            }
+        }
+        return sample;
     }
 
+    //swap the number in the valid row
+    private String[][] swapLine(String[][] sample){
+        for (int i = 0; i != 5; i++){
+            int row = rand.nextInt(9);
+            int changeRow = validChange(row);
+            String[] a = sample[row];
+            sample[row] = sample[changeRow];
+            sample[changeRow] = a;
+        }
+        return sample;
+    }
+
+    //swap the number in the valid column
+    private String[][] swapColumn(String[][] sample){
+        for (int i = 0; i != 5; i++){
+            int col = rand.nextInt(9);
+            int changeCol = validChange(col);
+            for(int row = 0; row != 9; row ++){
+                String a = sample[row][col];
+                sample[row][col] = sample[row][changeCol];
+                sample[row][changeCol] = a;
+            }
+        }
+        return sample;
+    }
+
+
+    // return the changable cols and rows
     private int validChange(int i){
-        Random rand = new Random();
         if (i % 3 == 0){
-            return rand.nextInt(i + 2) + (i + 1);
+            return rand.nextInt(2) + (i + 1);
         }
         else if ((i + 2) % 3 == 0){
-            return rand.nextInt(i + 1) + (i - 1);
+            return rand.nextInt(3) + (i - 1);
         }
         else{
-            return rand.nextInt(i - 1) + (i - 2);
+            return rand.nextInt(2) + (i - 2);
         }
     }
 
-//    private ArrayList<ArrayList<String>> getSampleSudoku(){
-//        ArrayList<ArrayList<String>> sample = new ArrayList<>();
-//        for (int i = 0; i < 9; i++) {
-//            List<String> a = Arrays.asList(seedSudoku[i]);
-//            sample.add((ArrayList<String>) a);
-//        }
-//        return sample;
-//    }
+    // Randomly clone a sudoku in seedsudokus
+    private String[][] getSampleSudoku(){
+        int random = rand.nextInt(seedSudokus.length);
+        return seedSudokus[random].clone();
+    }
+
+    // change a String[][] to a list
+    public static List<List<String>> getListSampleSudoku(String[][] clone){
+        List<List<String>> sample = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            List<String> a = Arrays.asList(clone[i]);
+            sample.add(a);
+        }
+        return sample;
+    }
 }
--- Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/SudokuManager.java
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/SudokuManager.java
@@ -1,7 +1,5 @@
 package fall2018.csc2017.slidingtiles;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -13,8 +11,13 @@ public class SudokuManager {
     private Set<String> correct = new HashSet<>(Arrays.asList(arr));
 
 
+
+    public boolean checkSudoku(List<List<String>> s){
+        return checkSquares(s) && (checkColumns(s) && checkRows(s));
+    }
+
     // check whether the all rows contains 1-9.
-    private boolean checkRows (ArrayList<ArrayList<String>> s) {
+    private boolean checkRows (List<List<String>> s) {
         for (int i = 0; i < 9; i++) {
             if (!checkRow(s.get(i))){
                 return false;
@@ -23,28 +26,47 @@ public class SudokuManager {
         return true;
     }
 
-    private void checkSquares (String[][] s){
 
+    private boolean checkSquares (List<List<String>> s){
+        for(int row = 0; row != 9; row += 3){
+            for(int col = 0; col != 9; col += 3){
+                if (!(checkSquare(row,col,s))){
+                    System.out.println("Square false");
+                    return false;
+
+                }
+            }
+        }
+        return true;
     }
-    private boolean checkColumns (ArrayList<ArrayList<String>> s){
+    private boolean checkSquare (int x, int y, List<List<String>> s){
+        Set<String> number = new HashSet<>();
+        for(int i = x;  i != x + 3; i++){
+            for (int j = y; j != y + 3; j++){
+                number.add(s.get(i).get(j));
+            }
+        }
+        return number.equals(correct);
+    }
+    private boolean checkColumns (List<List<String>> s){
         Set<String> c = new HashSet<>();
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 c.add(s.get(i).get(j));
             }
-            if (!(c == correct)){
+            if (!(c.equals(correct))){
                 return false;
             }
         }
         return true;
     }
 
-    private boolean checkRow(ArrayList<String> row){
+    private boolean checkRow(List<String> row){
         Set<String> r = new HashSet<>();
         for (int i = 0; i < 9; i++){
             r.add(row.get(i));
         }
-        return r == correct;
+        return r.equals(correct);
     }
     // return the rows or columns depends on the position of view of x or y
     private int changeToIndex(float viewX, float boxSide){
