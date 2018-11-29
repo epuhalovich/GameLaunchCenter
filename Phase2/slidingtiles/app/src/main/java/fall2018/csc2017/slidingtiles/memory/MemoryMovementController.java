58584f5507f4298a1217a7fb6d1b8e4926395c10
zhaoxuanzhong <xuanzhong.zhao@mail.utoronto.ca>
2018-11-29 16:50:54 -0500

    fixed errors.

--- Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/memory/MemoryMovementController.java
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/memory/MemoryMovementController.java
@@ -19,18 +19,20 @@ public class MemoryMovementController implements MovementController {
     }
 
     public void processTapMovement(Context context, int position, boolean display) {
-        if (memoryManager.isValidTap(position)) {
-            int numMatches = memoryManager.getNumMatches();
-            memoryManager.touchMove(position);
-            if (memoryManager.getNumMatches() > numMatches) {
-                Toast.makeText(context, "You got a match!", Toast.LENGTH_SHORT).show();
-            }
+        if (!(memoryManager == null)) {
+            if (memoryManager.isValidTap(position)) {
+                int numMatches = memoryManager.getNumMatches();
+                memoryManager.touchMove(position);
+                if (memoryManager.getNumMatches() > numMatches) {
+                    Toast.makeText(context, "You got a match!", Toast.LENGTH_SHORT).show();
+                }
 
-            if (memoryManager.isGameOver()) {
-                Toast.makeText(context, "YOU WIN!", Toast.LENGTH_SHORT).show();
+                if (memoryManager.isGameOver()) {
+                    Toast.makeText(context, "YOU WIN!", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(context, "Invalid Tap", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Toast.makeText(context, "Invalid Tap", Toast.LENGTH_SHORT).show();
         }
     }
 }
