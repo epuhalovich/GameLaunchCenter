bea6a3cb9add1ac230438a219cbe3f81a1477884
ellenpuhalovich <ellen.puhalovich@mail.utoronto.ca>
2018-11-30 10:56:46 -0500

    Added java doc

--- Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/gamecentre/PhaseTwoObserver.java
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/gamecentre/PhaseTwoObserver.java
@@ -2,6 +2,14 @@ package fall2018.csc2017.gamecentre;
 
 //Citation: https://www.journaldev.com/1739/observer-design-pattern-in-java
 public interface PhaseTwoObserver {
+    /**
+     * Update accordingly after subject calls notifyObservers()
+     */
     void update();
+
+    /**
+     * Set the subject to be obsevred
+     * @param subject to be observed
+     */
     void setSubject(PhaseTwoSubject subject);
 }
--- Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/gamecentre/PhaseTwoSubject.java
+++ Phase2/slidingtiles/app/src/main/java/fall2018/csc2017/gamecentre/PhaseTwoSubject.java
@@ -3,8 +3,11 @@ package fall2018.csc2017.gamecentre;
 //Citation :https://www.journaldev.com/1739/observer-design-pattern-in-java
 public interface PhaseTwoSubject {
 
+    /**
+     * Register the PhaseTwoObserver object to observe
+     * @param obj to register
+     */
      void register(PhaseTwoObserver obj);
-
 
     /**
      * A method to notifyObservers to change
